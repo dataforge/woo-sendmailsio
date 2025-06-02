cd37965a6a8633459660e51a2cd9110fb73c363b
RadialMonster <radial@gmail.com>
2025-06-02 08:57:56 -0400

    Fix indentation and update docblock for admin menu function
    Correct indentation in ABSPATH check and revise docblock to clarify the purpose of the admin menu function, improving code readability and documentation.

--- df-woocommerce-sendmails-io.php
+++ df-woocommerce-sendmails-io.php
@@ -9,10 +9,9 @@ GitHub Plugin URI: https://github.com/radialmonster/woocommerce-sendmails.io
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
-}
-
+    }
 /**
- * Add admin menu and submenus
+ * Handle adding a custom field to a list.
  */
 add_action('admin_menu', 'df_wc_sendmailsio_add_admin_menu');
 function df_wc_sendmailsio_add_admin_menu() {
